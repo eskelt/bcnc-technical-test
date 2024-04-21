952811ed0b22bcba470a3ae025063bc4f818997c
eskelt <diegogpr3@gmail.com>
2024-04-21 16:25:16 +0200

    Implement Open API generator, basic API spec and gitignore generated sources.

--- src/main/java/com/bcnc/test/bcnctest/rest/controller/ProductsController.java
+++ src/main/java/com/bcnc/test/bcnctest/rest/controller/ProductsController.java
@@ -1,13 +1,20 @@
 package com.bcnc.test.bcnctest.rest.controller;
 
+import com.bcnc.test.bcnctest.rest.api.ProductsApi;
+import com.bcnc.test.bcnctest.rest.dto.ProductsResponseDTO;
 import com.bcnc.test.bcnctest.service.in.ProductosService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-public class ProductsController {
+public class ProductsController implements ProductsApi {
 
     private final ProductosService productosService;
 
+    @Override
+    public ResponseEntity<ProductsResponseDTO> getProduct() {
+        return null;
+    }
 }
